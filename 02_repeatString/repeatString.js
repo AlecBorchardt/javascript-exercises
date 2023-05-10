a996a51a4515bb1b38696d1fb3b36225b01ae396
AlecBorchardt <alecborchardt@gmail.com>
2023-05-10 16:08:07 -0700

    Complete assignment.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let word = string;
+    for (let i = 1; i < num; i++){
+        string += word;
+    }
+    if (num < 0){
+        return ("ERROR");
+    }
+    else if (num == 0){
+        return "";
+    }
+    else {
+        return string;
+    }
 };
 
 // Do not edit below this line
