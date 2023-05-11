51b86b114d28dd7fdb3f27a5b3985620ab6d29c6
AlecBorchardt <alecborchardt@gmail.com>
2023-05-11 09:16:23 -0700

    complete assignment.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,29 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let letters = string.split("");
+    let reverse = [];
+for (let i = string.length; i >= 0; i--){
+    reverse.push(letters[i]);
+}
+
+
+
+    //function(){
+      //  for (let i = letters.length(); i < 0; )
+    //}
+
+
+    //string.split();
+    //string.length();
+    return reverse.join("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+/*
+Pseudocode.
+takes string, breaks it into pieces, then returns them in reverse order.
+
+
+*/
