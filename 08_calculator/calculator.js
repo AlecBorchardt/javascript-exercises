cdc3791c19a0efb57494cd75bfbc73d3a4ac0677
AlecBorchardt <alecborchardt@gmail.com>
2023-06-07 16:08:24 -0700

    complete assignment.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const initialValue = 0;
+  const sumWithInitial = array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+);
+return sumWithInitial;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayo) {
+  const initialValue = 1;
+  const sumWithInitial = arrayo.reduce(
+    (accumulator, currentValue) => accumulator * currentValue,
+    initialValue
+);
+return sumWithInitial;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(foo) {
+ let aryay = [];
+ for (let i = foo; i > 1; i--) {
+  aryay.push(i);
+ }
+ let initial = 1;
+return aryay.reduce(
+  (acc,val) => acc * val, initial
+  )
 };
 
 // Do not edit below this line
