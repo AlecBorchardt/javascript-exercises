044a3aae884d46681eeaac5e24865edded90e927
AlecBorchardt <alecborchardt@gmail.com>
2023-06-08 10:31:57 -0700

    complete assignmnet.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    
+        let arr = [];
+        let len = books.length;
+        
+        for(let i = (len - 1); i >= 0; i--){
+        arr.push(books[i].title);
+        }
+        return arr.reverse();
+        }
 
 // Do not edit below this line
 module.exports = getTheTitles;
