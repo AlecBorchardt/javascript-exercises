4ff8418a0463cdc41668079a8de640d1585d38a8
AlecBorchardt <alecborchardt@gmail.com>
2023-06-08 09:46:56 -0700

    complete assignment.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
 
+    Number(fibNum);
+
+    let fibArray = [1];
+    
+    let adder = function(a, b) {
+      return a + b;
+    };
+    
+if(fibNum > 0){
+
+    for (let i = fibNum; i > 1; i--) {
+      let tempArray = fibArray.slice(Math.max(fibArray.length - 2, 0));
+      let tempCalc = tempArray.reduce(adder);
+      fibArray.push(tempCalc);
+
+      console.log(tempArray);
+      console.log(tempCalc);
+      console.log(fibArray);
+    }
+
+return fibArray[(fibNum-1)];
+}
+
+else {
+return "OOPS";
+}
 };
 
 // Do not edit below this line
