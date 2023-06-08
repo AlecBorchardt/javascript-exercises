011611192358ab7d5d09cebc9a4865ef1a7d18a9
AlecBorchardt <alecborchardt@gmail.com>
2023-06-08 08:12:08 -0700

    complete assignment.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function(stringy) {
 
-};
+    stringy = stringy.toLowerCase().replace(/[.,\/#!$%\^&\*;! :{}=\-_`~()]/g,"");
+
+let strungy = stringy.split('');
+
+let strangy = strungy.reverse().join("");
+
+console.log(stringy);
+console.log(strangy);
+
+if (stringy == strangy){
+return true;
+}
+else {
+return false;
+}
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
